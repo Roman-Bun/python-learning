91582df87050547f2154123e711543fe0d851cec
Roman Bun <rbun1992@gmail.com>
2026-04-08 13:31:00 +0300

    add data_types.py

---
+++ data_types.py
@@ -0,0 +1,17 @@
+# 1. Створи змінні для контакту:
+name = str(input("Введіть ім'я: "))
+age = int(input("Введіть вік: "))
+height = float(input("Введіть зріст: "))
+is_friend = True
+
+# 2. Виведи кожну змінну через print()
+print(name)
+print(age)
+print(height)
+print(is_friend)
+
+# 3. Виведи тип кожної змінної через type()
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_friend))
